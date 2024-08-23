b73cb3d78fde3b3799a1c1f4a23df8afb33ac3ea
tomatteo <133718412+tomatteo@users.noreply.github.com>
2024-08-23 11:29:14 -0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -30,6 +30,37 @@ void main() async {
         "Coloque os ovos em uma panela e colque agua até cobrir os ovos leve a panela com os ovos ao fogo e ferva os ovos por 7 minutos.",
   );
 
+  Receita miojo = Receita(
+    id: 3,
+    nome: "Miojo",
+    image: "assets/images/miojo.png",
+    ingredientes: "1 Miojo e água",
+    modopreparo:
+        "Abra o pacote de miojo e coloque-o em uma panela, adicione água e leve ao fogo por 5 minutos .",
+  );
+
+  Receita pipocasal = Receita(
+    id: 4,
+    nome: "Pipoca Salgada",
+    image: "assets/images/pipocasal.png",
+    ingredientes:
+        "2 colheres (sopa) de óleo (30ml), 1 xícara de milho (cerca de 200 ml) de pipoca e cerca de 1 colher (chá) de sal (5 ml)",
+    modopreparo:
+        "Coloque o óleo em uma panela grande e ligue em fogo alto quando o óleo estiver bem quente adicione o milho, tampe a panela e chacoalhe um pouco e coloque em fogo médio e deixe o milho estourar, quando não ouvir mais estouros, desligue o fogo Adicione o sal e chacoalhe bem coloque a pipoca em uma vasilha e aproveite.",
+  );
+
+  Receita piraoagua = Receita(
+    id: 5,
+    nome: "Pirão de Água",
+    image: "assets/images/piraoagua.png",
+    ingredientes:
+        "2 xícaras de cafezinho de farinha de mandioca 1 colher de cafezinho de sal 500 ml de água 1 colher de sopa de manteiga",
+    modopreparo:
+        "Coloque todos os ingredientes em uma panela e leve ao fogo médio, mexa sempre, até que forme uma massa inteira e consistente, assim virando um pirão! Deve durar cerca de 5 a 7 minutos. ",
+  );
+  await insertReceita(piraoagua);
+  await insertReceita(pipocasal);
+  await insertReceita(miojo);
   await insertReceita(coca);
   await insertReceita(ovoCozido);
 //fim da add receitas
@@ -133,7 +164,63 @@ class MyApp extends StatelessWidget {
                       }
                     },
                   ),
-                  // Receita Ovo Cozido
+
+                  FutureBuilder(
+                    future: findReceitaById(4),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return CircularProgressIndicator();
+                      } else if (snapshot.hasError) {
+                        return Text('Erro ao carregar receita');
+                      } else if (snapshot.hasData) {
+                        Receita receita = snapshot.data as Receita;
+                        return Column(
+                          children: [
+                            Text(
+                              receita.nome,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 30,
+                              ),
+                            ),
+                            Container(
+                              margin:
+                                  EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                              child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  child: Image.asset(receita.image)),
+                            ),
+                            Center(
+                              child: TextButton(
+                                child: Text(
+                                  "Ver Receita",
+                                  style: TextStyle(fontSize: 17),
+                                ),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          TelaReceita(receita: receita),
+                                    ),
+                                  );
+                                },
+                                style: TextButton.styleFrom(
+                                    foregroundColor: const Color.fromARGB(
+                                        255, 255, 255, 255),
+                                    backgroundColor:
+                                        Color.fromRGBO(113, 93, 74, 0.807)),
+                              ),
+                            ),
+                          ],
+                        );
+                      } else {
+                        return Text('Nenhuma receita encontrada');
+                      }
+                    },
+                  ),
+                  // receita ovo
                   FutureBuilder(
                     future: findReceitaById(2),
                     builder: (context, snapshot) {
@@ -145,8 +232,64 @@ class MyApp extends StatelessWidget {
                         Receita receita = snapshot.data as Receita;
                         return Column(
                           children: [
+                            Text(
+                              receita.nome,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 30,
+                              ),
+                            ),
+                            Container(
+                              margin:
+                                  EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                              child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  child: Image.asset(receita.image)),
+                            ),
+                            Center(
+                              child: TextButton(
+                                child: Text(
+                                  "Ver Receita",
+                                  style: TextStyle(fontSize: 17),
+                                ),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          TelaReceita(receita: receita),
+                                    ),
+                                  );
+                                },
+                                style: TextButton.styleFrom(
+                                    foregroundColor: const Color.fromARGB(
+                                        255, 255, 255, 255),
+                                    backgroundColor:
+                                        Color.fromRGBO(113, 93, 74, 0.807)),
+                              ),
+                            ),
+                          ],
+                        );
+                      } else {
+                        return Text('Nenhuma receita encontrada');
+                      }
+                    },
+                  ),
+                  // Receita miojo
+                  FutureBuilder(
+                    future: findReceitaById(3),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return CircularProgressIndicator();
+                      } else if (snapshot.hasError) {
+                        return Text('Erro ao carregar receita');
+                      } else if (snapshot.hasData) {
+                        Receita receita = snapshot.data as Receita;
+                        return Column(
+                          children: [
                             Padding(
-                              padding: EdgeInsets.only(top: 40.0),
+                              padding: EdgeInsets.only(top: 10.0),
                               child: Text(
                                 receita.nome,
                                 textAlign: TextAlign.center,
@@ -154,6 +297,61 @@ class MyApp extends StatelessWidget {
                                   color: Colors.black,
                                   fontSize: 30,
                                 ),
+                              ),
+                            ),
+                            Container(
+                              margin:
+                                  EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                              child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  child: Image.asset(receita.image)),
+                            ),
+                            Center(
+                              child: TextButton(
+                                child: Text(
+                                  "Ver Receita",
+                                  style: TextStyle(fontSize: 17),
+                                ),
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          TelaReceita(receita: receita),
+                                    ),
+                                  );
+                                },
+                                style: TextButton.styleFrom(
+                                    foregroundColor: const Color.fromARGB(
+                                        255, 255, 255, 255),
+                                    backgroundColor:
+                                        Color.fromRGBO(113, 93, 74, 0.807)),
+                              ),
+                            ),
+                          ],
+                        );
+                      } else {
+                        return Text('Nenhuma receita encontrada');
+                      }
+                    },
+                  ),
+                  FutureBuilder(
+                    future: findReceitaById(5),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return CircularProgressIndicator();
+                      } else if (snapshot.hasError) {
+                        return Text('Erro ao carregar receita');
+                      } else if (snapshot.hasData) {
+                        Receita receita = snapshot.data as Receita;
+                        return Column(
+                          children: [
+                            Text(
+                              receita.nome,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 30,
                               ),
                             ),
                             Container(
@@ -230,22 +428,31 @@ class TelaReceita extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(receita.nome,
-            style: GoogleFonts.montserrat(fontSize: 25, color: Colors.white)),
+        title: Text(
+          receita.nome,
+          style: GoogleFonts.montserrat(fontSize: 25, color: Colors.white),
+        ),
         backgroundColor: Color.fromARGB(255, 0, 0, 0),
         elevation: 0,
         centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: Color.fromARGB(255, 251, 251, 251),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: ListView(
           children: [
             Container(
               margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
               child: ClipRRect(
-                  borderRadius: BorderRadius.circular(15.0),
-                  child: Image.asset(receita.image)),
+                borderRadius: BorderRadius.circular(15.0),
+                child: Image.asset(receita.image),
+              ),
             ),
             Text(
               'Ingredientes:',
